93388b736dbdf8c85545cd1f51c16127c882546a
Nadin <nadyusha-serdyuk@inbox.ru>
2023-04-06 16:31:20 +0300

    done

--- ДЗ 4/task 2/my.sql
+++ ДЗ 4/task 2/my.sql
@@ -1,5 +1,5 @@
 -- create
-CREATE TABLE Студенты (
+CREATE TABLE Students (
   empId INTEGER PRIMARY KEY AUTO_INCREMENT,
   name TEXT NOT NULL,
   age TEXT NOT NULL,
@@ -7,20 +7,20 @@ CREATE TABLE Студенты (
 );
 
 -- insert
-INSERT INTO Студенты (name, age, address) VALUES ('Алексей', '36', 'Москва');
-INSERT INTO Студенты (name, age, address) VALUES ('Никита', '23', 'Саратов');
-INSERT INTO Студенты (name, age, address) VALUES ('Зарина', '40', 'Ижевск');
-INSERT INTO Студенты (name, age, address) VALUES ('Елена', '28', 'Москва');
-INSERT INTO Студенты (name, age, address) VALUES ('Федор', '32', 'Москва');
-INSERT INTO Студенты (name, age, address) VALUES ('Екатерина', '40', 'Казань');
-INSERT INTO Студенты (name, age, address) VALUES ('Альберт', '37', 'Саратов');
-INSERT INTO Студенты (name, age, address) VALUES ('Семён', '20', 'Москва');
+INSERT INTO Students (name, age, address) VALUES ('Алексей', '36', 'Москва');
+INSERT INTO Students (name, age, address) VALUES ('Никита', '23', 'Саратов');
+INSERT INTO Students (name, age, address) VALUES ('Зарина', '40', 'Ижевск');
+INSERT INTO Students (name, age, address) VALUES ('Елена', '28', 'Москва');
+INSERT INTO Students (name, age, address) VALUES ('Федор', '32', 'Москва');
+INSERT INTO Students (name, age, address) VALUES ('Екатерина', '40', 'Казань');
+INSERT INTO Students (name, age, address) VALUES ('Альберт', '37', 'Саратов');
+INSERT INTO Students (name, age, address) VALUES ('Семён', '20', 'Москва');
 
 -- fetch 
 SELECT
 empId as "Id",
 name as "Имя"
-FROM Студенты
+FROM Students
 WHERE address = 'Москва'
 AND age BETWEEN 18 AND 30
 ORDER BY name;
